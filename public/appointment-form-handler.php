35bb6f08c763ef664dcb954d93b1f9a584e513a6
unknown <puneethnviraat@gmail.com>
2022-10-07 20:21:11 +0530

    appointment form handler

---
+++ public/appointment-form-handler.php
@@ -0,0 +1,62 @@
+<?php 
+$errors = '';
+$myemail = 'canshospital1@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['first_name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['phone']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: Please fill the requied fileds";
+}
+
+$name = $_POST['first_name']; 
+$phone = $_POST['phone']; 
+$email_address = $_POST['email']; 
+$department = $_POST['department']; 
+$doctor = $_POST['doctor']; 
+$date = $_POST['date']; 
+$time = $_POST['time']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "New Appointment booking: $name";
+	$email_body = "You have received a new appointment. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Phone: $phone \n
+	Department: $department \n
+	Doctor: $doctor \n
+	Date: $date \n
+	Time: $time \n
+	 Message: \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
