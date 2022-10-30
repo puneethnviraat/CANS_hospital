12ecdbc2cd888292ce4118a9b40cea88d369fec0
unknown <puneethnviraat@gmail.com>
2022-10-30 20:31:02 +0530

    appointment changes

--- public/appointment-form-handler.php
+++ public/appointment-form-handler.php
@@ -1,50 +1,53 @@
-<?php
-
-$EmailTo = "canshospital1@gmail.com";
-$Subject = "New appointment Request: $name";
-
-$success = false;
-$errorMSG = array();
-$name = $name = $email = $phone=  $message = null;
-$fields = array(
-    'name' => "First name is required ",
-    'email' => "Email is required ",
-    'phone' => "Phone is required ",
-    'message' => "Message is required "
-);
-
-foreach($fields as $key => $e_message){
-    if (empty($_POST[$key])) {
-        $errorMSG[] = $e_message;
-    } else {
-        $$key = $_POST[$key];
-    }
+<?php 
+$errors = '';
+$myemail = 'canshospital1@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['phone']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: Please fill the requied fileds";
 }
 
-$name = $name ;
+$name = $_POST['name']; 
+$phone = $_POST['phone']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
 
-// prepare email body text
-$Body = null;
-$Body .= "<p><b>Name:</b> {$name}</p>";
-$Body .= "<p><b>Email:</b> {$email}</p>";
-$Body .= "<p><b>Phone:</b> {$phone}</p>";
-$Body .= "<p><b>Message:</b> </p><p>{$message}</p>";
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
 
-// send email
-$headers = "From: $myemail\n"; 
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Appointment request: $name";
+	$email_body = "You have received a new appointment request. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Phone: $phone \n Message:  $message"; 
+	
+	$headers = "From: $email_address\n"; 
 	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
 
-if($name && $email && $message){
-    $success = mail($EmailTo, $Subject, $Body, $headers );
-}
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
 
-if(empty($errorMSG)){
-    $errorMSG[] = "Something went wrong :(";
-}
 
-echo json_encode(array(
-    'success' => $success,
-    'message' => $errorMSG
-));
-
-die();
+</body>
+</html>
